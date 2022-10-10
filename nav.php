f22f4948d283bbf09141ee8da04a0bb72efff436
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-10-10 14:01:40 -0500

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="course.php">Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="majors.php">Majors</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="buildings.php">Buildings</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="student.php">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="student-cards.php">Courselist with Enrolled Students</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
