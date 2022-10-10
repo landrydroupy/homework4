6e5efbcb4399a5ced696b89b4b32d3b2fa879b48
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-10-10 14:03:27 -0500

    Create student.php

---
+++ student.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<?php include("nav.php");?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Courses</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Students</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>studentid</th>
+      <th>courseid</th>
+      <th>fname</th>
+      <th>lname</th>
+      <th>grade</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "landryou_user";
+$password = "A2kYbmhiMHTE";
+$dbname = "landryou_homework3data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * from student";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["studentid"]?></td>
+    <td><?=$row["courseid"]?></td>
+    <td><?=$row["fname"]?></td>
+    <td><?=$row["lname"]?></td>
+    <td><?=$row["grade"]?></td>
+    <td>
+      <form method="post" action="student-course.php">
+        <input type="hidden" name="id" value="<?=$row["courseid"]?>" />
+        <input type="submit" value="View Course" />
+      </form>
+    </td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
