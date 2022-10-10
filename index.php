88f7aff34438c7427dfe52300d50c9141f7c4f11
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-10-10 13:58:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<!doctype html>
+<?php include("nav.php");?>
 
+<h1 style="text-align:center"> Welcome to my database website, this will contain 4 separate tables describing different aspects of The University of Oklahoma.<h1>
+
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
