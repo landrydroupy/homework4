4f27aa1e3a59a0ea85aa8100f6d65c601abd5fc3
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-10-19 14:21:31 -0500

    Update majors-add-save.php

--- majors-add-save.php
+++ majors-add-save.php
@@ -1,1 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Major</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<?php
+$servername = "localhost";
+$username = "landryou_user";
+$password = "A2kYbmhiMHTE";
+$dbname = "landryou_homework3data";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$mName = $_POST['mName'];
+
+$sql = "insert into majors (majorName) value (?)";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $mName);
+    $stmt->execute();
+?>
+    
+    <h1>Add Major</h1>
+<div class="alert alert-success" role="alert">
+  New Major added.
+</div>
+    <a href="majors.php" class="btn btn-primary">Go back</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
