e42aaf3f4e7591176a06aa8efa74a9c83aa2c482
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-10-24 13:47:01 -0500

    Update majors.php

--- majors.php
+++ majors.php
@@ -1,27 +1,15 @@
 <!doctype html>
-<?php include("nav.php");?>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Courses</title>
+    <title>Buildings</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Majors</h1>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>majorid</th>
-      <th>majorName</th>
-      <th>majorDescription</th>
+    <div class="container">
       
-     
-      
-    </tr>
-  </thead>
-  <tbody>
-    <?php
+<?php
 $servername = "localhost";
 $username = "landryou_user";
 $password = "A2kYbmhiMHTE";
@@ -33,23 +21,94 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "SELECT * from majors";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+      $sqlAdd = "insert into majors (majorName) value (?)";
+      $stmtAdd = $conn->prepare($sqlAdd);
+      $stmtAdd->bind_param("s", $_POST['mName']);
+      $stmtAdd->execute();
+      echo '<div class="alert alert-success" role="alert">New Major added.</div>';
+      break;
+    case 'Edit':
+      $sqlEdit = "update majors set majorName=? where majorid=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("si", $_POST['mName'], $_POST['mid']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Major edited.</div>';
+      break;
+    case 'Delete':
+      $sqlDelete = "delete from majors where majorid=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("i", $_POST['mid']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">Major deleted.</div>';
+      break;
+  }
+}
+?>
+    
+      <h1>Majors</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>majorid</th>
+            <th>majorName</th>
+           <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          
+<?php
+$sql = "SELECT majorid,majorName from majors";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <tr>
-    <td><?=$row["majorid"]?></td>
-    <td><?=$row["majorName"]?></td>
-    <td><?=$row["majorDescription"]?></td>
-    
-    <td>
-      
-    </td>
-  </tr>
+          
+          <tr>
+           <td><?=$row["majorid"]?></td>
+            <td><?=$row["majorName"]?></td>
+            <td>
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editMajor<?=$row["majorid"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editMajor<?=$row["majorid"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editMajor<?=$row["majorid"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editMajor<?=$row["majorid"]?>Label">Edit Major</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editMajor<?=$row["majorid"]?>name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editMajor<?=$row["majorid"]?>Name" aria-describedby="editMajor<?=$row["majorid"]?>Help" name="mName" value="<?=$row['majorName']?>">
+                          <div id="editMajor<?=$row["majorid"]?>Help" class="form-text">Enter the major name.</div>
+                        </div>
+                        <input type="hidden" name="mid" value="<?=$row['majorid']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="mid" value="<?=$row["majorid"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+              </form>
+            </td>
+          </tr>
+          
 <?php
   }
 } else {
@@ -57,8 +116,38 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </tbody>
-    </table>
+          
+        </tbody>
+      </table>
+      <br />
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addMajor">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addMajor" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addMajorLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addMajorLabel">Add Major</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="majorName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="majorName" aria-describedby="nameHelp" name="mName">
+                  <div id="nameHelp" class="form-text">Enter the major name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
