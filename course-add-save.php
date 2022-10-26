25f68fc286bee2f5b0873da69e3fdca5a253990b
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-10-26 10:24:06 -0500

    Create course-add-save.php

---
+++ course-add-save.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Course</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<?php
+$servername = "localhost";
+$username = "landryou_user";
+$password = "A2kYbmhiMHTE";
+$dbname = "landryou_homework3data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$cPrefix = $_POST['cPrefix'];
+$cNumber= $_POST['cNumber'];
+$cDescription= $_POST['cDescription'];
+$sql = "insert into course (prefix,number,description) value (?,?,?)";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $cPrefix,$cNumber,$cDescription);
+    $stmt->execute();
+?>
+    
+    <h1>Add Course</h1>
+<div class="alert alert-success" role="alert">
+  New Course added.
+</div>
+    <a href="course.php" class="btn btn-primary">Go back</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
